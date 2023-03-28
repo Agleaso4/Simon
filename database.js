7f8a36f0f4d3499c66de818409f3008f2665c1dd
agleaso4 <115682702+AmmonArctech@users.noreply.github.com>
2023-03-28 06:43:20 -0600

    Updated the communciation for WebSocket

--- database.js
+++ database.js
@@ -2,9 +2,9 @@ const { MongoClient } = require('mongodb');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 
-const userName = process.env.MONGOUSERSIMON;
-const password = process.env.MONGOPASSWORDSIMON;
-const hostname = process.env.MONGOHOSTNAMESIMON;
+const userName = process.env.MONGOUSER;
+const password = process.env.MONGOPASSWORD;
+const hostname = process.env.MONGOHOSTNAME;
 
 if (!userName) {
   throw Error('Database not configured. Set environment variables');
